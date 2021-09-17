8270a926118e64ecf1f4387cc34f0f57acf42392
Nicolas Bevilacqua <nicobevilacqua@gmail.com>
2021-09-17 12:11:46 -0300

    small fixes

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,21 +1,22 @@
 import { config } from 'dotenv';
 config();
 
-import { task } from 'hardhat/config';
 import '@nomiclabs/hardhat-etherscan';
 import '@nomiclabs/hardhat-waffle';
 import 'hardhat-gas-reporter';
 import 'solidity-coverage';
 import 'hardhat-watcher';
+import { task } from 'hardhat/config';
+import path from 'path';
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
-	const accounts = await hre.ethers.getSigners();
+  const accounts = await hre.ethers.getSigners();
 
-	for (const account of accounts) {
-		console.log(account.address);
-	}
+  for (const account of accounts) {
+    console.log(account.address);
+  }
 });
 
 // You need to export an object to set up your config
@@ -25,44 +26,50 @@ task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 export default {
-	solidity: '0.8.4',
+  solidity: {
+    version: '0.8.4',
+    settings: {
+      optimizer: {
+        enabled: !!process.env.OPTIMIZER_ENABLED,
+        runs: 1000,
+      },
+    },
+  },
 
-	networks: {
-		hardhat: {
-			initialBaseFeePerGas: 0, // workaround from https://github.com/sc-forks/solidity-coverage/issues/652#issuecomment-896330136 . Remove when that issue is closed.
-		},
+  networks: {
+    hardhat: {
+      initialBaseFeePerGas: 0, // workaround from https://github.com/sc-forks/solidity-coverage/issues/652#issuecomment-896330136 . Remove when that issue is closed.
+    },
 
-		ropsten: {
-			url: process.env.ROPSTEN_URL || '',
-			accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-		},
+    rinkeby: {
+      url: process.env.RINKEBY_URL || '',
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
 
-		rinkeby: {
-			url: process.env.RINKEBY_URL || '',
-			accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-		},
-	},
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    gasPrice: 21,
+    coinmarketcap: process.env.CMC_KEY,
+    currency: 'USD',
+    outputFile: process.env.TO_FILE ? path.resolve(__dirname, 'gasReporterOutput.json') : undefined,
+  },
 
-	gasReporter: {
-		enabled: process.env.REPORT_GAS !== undefined,
-		currency: 'USD',
-	},
+  watcher: {
+    compile: {
+      tasks: ['compile'],
+      files: ['./contracts'],
+      verbose: true,
+    },
 
-	watcher: {
-		compile: {
-			tasks: ['compile'],
-			files: ['./contracts'],
-			verbose: true,
-		},
+    test: {
+      tasks: [{ command: 'test', params: { testFiles: ['{path}'] } }],
+      files: ['./test/**/*'],
+      verbose: true,
+    },
+  },
 
-		test: {
-			tasks: [{ command: 'test', params: { testFiles: ['{path}'] } }],
-			files: ['./test/**/*'],
-			verbose: true,
-		},
-	},
-
-	etherscan: {
-		apiKey: process.env.ETHERSCAN_API_KEY,
-	},
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
