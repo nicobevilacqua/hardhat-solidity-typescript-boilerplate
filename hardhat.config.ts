c7cd94f888b58136f4ea83815f32b7f5d91191e2
Nicolás Bevilacqua <nicobevilacqua@gmail.com>
2022-08-29 22:06:39 -0300

    hardhat-toolbox added

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,26 +1,12 @@
+import '@nomicfoundation/hardhat-toolbox';
 import { config } from 'dotenv';
 config();
 
 import '@nomiclabs/hardhat-etherscan';
-import '@nomiclabs/hardhat-waffle';
 import 'hardhat-gas-reporter';
 import 'solidity-coverage';
 import 'hardhat-watcher';
-import { task } from 'hardhat/config';
 import path from 'path';
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
